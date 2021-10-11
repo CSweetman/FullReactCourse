de64bdd736e6af637fd9f4912845172398a43736
CSweetman <chrisjdsweetman@gmail.com>
2021-10-11 12:25:44 -0500

    Mapped lists of objects as componets and destructruing props. Event Basics of onClick and onMouseOver. Next will be import and export, how to host and starting the project to cover advance topics

--- src/index.tsx
+++ src/index.tsx
@@ -1,40 +1,66 @@
 import * as React from 'react';
 import ReactDOM from 'react-dom';
-import { Component } from 'react';
+import { Component, MouseEvent } from 'react';
 
 // CSS
 import './index.css'
 //set up vars
-const firstBook = {
-  author: "Eric Cale",
-  title: 'The Very Hungry Caterpillar',
-  img: "https://images-na.ssl-images-amazon.com/images/I/41tyokViuNL._SY355_BO1,204,203,200_.jpg"
-}
+const books = [
+  {
+    id: 1,
+    author: "Eric Cale",
+    title: 'The Very Hungry Caterpillar',
+    img: "https://images-na.ssl-images-amazon.com/images/I/41tyokViuNL._AC_SX184_.jpg"
+  },
+  {
+    id: 2,
+    author: "Amelia Hepworth",
+    title: 'I Love You to the Moon and Back',
+    img: "https://images-na.ssl-images-amazon.com/images/I/517h-u1AQlL._AC_SX184_.jpg"
+  },
+  {
+    id: 3,
+    author:'Bill Martin Jr.',
+    title: 'Brown Bear, Brown Bear, What Do You See?',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/51430n+9jlL._AC_SX184_.jpg'
+  },
+]
 
-const secondBook = {
-  author: "Amelia Hepworth",
-  title: 'I Love You to the Moon and Back',
-  img: "https://images-na.ssl-images-amazon.com/images/I/517h-u1AQlL._SX482_BO1,204,203,200_.jpg"
-}
 
 function BookList(){
   return( 
     <section className = "booklist">
-      <Book img={firstBook.img} title={firstBook.title} author = {firstBook.author}>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque ratione error id iusto consectetur impedit voluptatibus provident rem, doloribus quis!</p>
-      </Book>
-      <Book title={secondBook.title} author = {secondBook.author} img={secondBook.img}/>
+    {books.map( (book) => {
+      return(
+        <Book key={book.id} {...book}></Book>
+      )
+    })}
     </section>
   )
 }
 
-const Book = ({img, title, author, children}: any) :JSX.Element => {
-  //const {img, title, author} = props
-  return <article className = "book">
+
+const Book: React.FunctionComponent = ({img, title, author}: any)  => {
+  // attribute, eventhandler
+  //onClick, onMouseOver
+  const clickHandler = () =>{
+    alert(title)
+  }
+
+  const complexExample = (output: String) =>{
+    console.log(output)
+  }
+
+  return <article className = "book" onMouseOver={() =>{
+    console.log(title)
+  }}>
     <img src={img} alt="" />
     <h1>{title}</h1>
     <h4>{author.toUpperCase()}</h4>
-    {children}
+    <button type='button' onClick={clickHandler}>reference example</button>
+    <button type='button' onClick={ () =>
+      complexExample(title)
+    }>More complex example</button>
   </article>
 } 
 
